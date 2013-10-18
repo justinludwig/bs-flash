ca8f1d4ceb5beea5c5a1a9e06f7a74665778eb37
Justin Ludwig <justin@pitchstonetechnology.com>
2013-10-18 09:53:55 -0700

    implement flash-serializing cookie

---
+++ grails-app/utils/com/pitchstone/plugin/flash/BasicSessionlessFlashCookie.groovy
@@ -0,0 +1,223 @@
+package com.pitchstone.plugin.flash
+
+import java.security.Key
+import java.text.SimpleDateFormat
+import javax.crypto.Mac
+import javax.servlet.http.Cookie
+import org.apache.commons.logging.Log
+
+class BasicSessionlessFlashCookie extends Cookie {
+
+    BasicSessionlessFlashCookie() {
+        super('null', null)
+    }
+
+    BasicSessionlessFlashCookie(Cookie cookie) {
+        this()
+
+        if (!cookie) return
+        '''
+            comment domain maxAge name path secure value version
+            httpOnly flash macAlgorithm secret log
+        '''.trim().split(/\s+/).findAll {
+            // don't try to access if not available (eg httpOnly)
+            // or if null (eg domain)
+            cookie.hasProperty(it) && cookie[it]
+        }.each {
+            this[it] = cookie[it]
+        }
+    }
+
+    // Cookie
+
+    Object clone() {
+        new BasicSessionlessFlashCookie(this)
+    }
+
+    /** Add setter to name. */
+    String name = ''
+
+    String getValue(long now = 0) {
+        if (!super.value && flash)
+            value = encodeFlash(flash, now)
+        return super.value ?: ''
+    }
+
+    // impl
+
+    /** Pre-servlet-3.0 compatability. */
+    boolean httpOnly
+
+    Map<String,String> flash = [:]
+    Map<String,String> getFlash(long now = 0) {
+        if (!flash && super.value)
+            flash = decodeValue(super.value, maxAge, now)
+        return flash ?: [:]
+    }
+
+    String macAlgorithm
+    Key secret
+    Log log
+    boolean doubleEncode
+
+    /**
+     * Sets this cookie on the specified response.
+     */
+    void send(response, long now = 0) {
+        // write header manually for pre-servlet-3.0 compatability
+        def parts = [ "${name}=${getValue(now)?:''}" ]
+        if (maxAge >= 0)
+            parts << "expires=${getFormattedMaxAge(now)}"
+        if (domain)
+            parts << "domain=${domain}"
+        if (path)
+            parts << "path=${path}"
+        if (secure)
+            parts << 'secure'
+        if (httpOnly)
+            parts << 'httponly'
+        response.addHeader 'Set-Cookie', parts.join('; ')
+    }
+
+    /**
+     * Sets this cookie on the specified response in a way that will clear it.
+     */
+    void cancel(response) {
+        maxAge = 0
+        if (!path)
+            path = '/'
+        flash = [:]
+        response.addCookie this
+    }
+
+    /**
+     * Returns a string formatted as this cookie's 'expires' date.
+     */
+    String getFormattedMaxAge(long now = 0) {
+        formatMaxAge maxAge, now
+    }
+
+    /**
+     * Returns a string formatted as a cookie 'expires' date
+     * the specified seconds in the future.
+     */
+    String formatMaxAge(Integer maxAge, long now = 0) {
+        def ms = maxAge ? (now ?: System.currentTimeMillis()) + maxAge * 1000 : 0
+        formatDate new Date(ms)
+    }
+
+    /**
+     * Returns a string formatted as a cookie 'expires' date
+     * with the specified date.
+     */
+    String formatDate(Date date) {
+        def pattern = "EEE, dd-MMM-yyyy HH:mm:ss 'GMT'"
+        def formatter = new SimpleDateFormat(pattern, Locale.US)
+        formatter.timeZone = TimeZone.getTimeZone('GMT')
+        formatter.format date
+    }
+
+    /**
+     * Serializes the specified flash-map into the cookie value.
+     * Returns a empty string if the map is null or empty.
+     */
+    String encodeFlash(Map<String,String> flash, long now = 0) {
+        if (!flash) return ''
+
+        def encoder = new URLEncoder()
+        def parts = []
+        flash.each { k,v -> 
+            parts << encoder.encode(k as String, 'UTF-8')
+            parts << ':'
+            parts << encoder.encode(v as String, 'UTF-8')
+            parts << '|'
+        }
+        parts << formatRfc3339(new Date(now ?: System.currentTimeMillis()))
+        def mac = calculateMac(parts.join('').getBytes('ASCII'))
+        parts << '|' << mac
+
+        def value = parts.join('')
+        // optionally encode entire cookie with base64
+        // to protect against faulty cookie handling
+        doubleEncode ? 
+            value.getBytes('ASCII').encodeAsBase64().replaceAll(/=+$/, '') : value
+    }
+
+    /**
+     * Deserializes the specified cookie value into a flash-map.
+     * Returns an empty map if the cookie is null, empty, or invalid.
+     */
+    Map<String,String> decodeValue(String value, maxAge = 0, long now = 0) {
+        if (!value) return [:]
+
+        // reverse extra base64 encoding
+        if (doubleEncode)
+            value = new String(value.decodeBase64(), 'ASCII')
+
+        def parts = value.split(/\|/) as List
+        if (parts.size < 3) {
+            log.warn "not enough parts for flash cookie: $value"
+            return [:]
+        }
+
+        def submittedMac = parts.pop()
+        def calculatedMac = calculateMac(parts.join('|').getBytes('ASCII'))
+        if (submittedMac != calculatedMac) {
+            log.warn "invalid mac for flash cookie: $value"
+            return [:]
+        }
+
+        def ms = parseRfc3339(parts.pop())?.time ?: 0
+        if (ms < (now ?: System.currentTimeMillis()) - maxAge * 1000) {
+            log.warn "too-old flash cookie: $value"
+            return [:]
+        }
+
+        def decoder = new URLDecoder()
+        parts.findAll { it ==~ /[^:]+:[^:]+/ }.inject([:]) { map, part ->
+            def (k, v) = part.split(/:/)
+            k = decoder.decode(k, 'UTF-8')
+            v = decoder.decode(v, 'UTF-8')
+            map[k] = v
+            return map
+        }
+    }
+
+    /**
+     * Generates base64-encoded MAC for the specified message.
+     */
+    String calculateMac(byte[] message) {
+        Mac mac = Mac.getInstance(macAlgorithm)
+        mac.init(secret)
+        mac.doFinal(message).encodeAsBase64().replaceAll(/=+$/, '')
+    }
+
+    /**
+     * Formats the specified date in RFC 3339 style.
+     * Returns null if the specified date is null.
+     */
+    String formatRfc3339(Date date, boolean ms = true) {
+        if (!date) return null
+
+        def pattern = "yyyy-MM-dd'T'HH:mm:ss${ms?'.SSS':''}'Z'"
+        def formatter = new SimpleDateFormat(pattern, Locale.US)
+        formatter.timeZone = TimeZone.getTimeZone('GMT')
+        formatter.format date 
+    }
+
+    /**
+     * Parses the specified date in RFC 3339 style.
+     * Returns null if the specified string cannot be parsed.
+     */
+    Date parseRfc3339(String s) {
+        def m = s =~ /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z/
+        if (!m) return null
+
+        def ms = m[0][1]
+        def pattern = "yyyy-MM-dd'T'HH:mm:ss${ms?'.SSS':''}'Z'"
+        def formatter = new SimpleDateFormat(pattern, Locale.US)
+        formatter.timeZone = TimeZone.getTimeZone('GMT')
+        formatter.parse s
+    }
+
+}
---
+++ test/unit/com/pitchstone/plugin/flash/BasicSessionlessFlashCookieSpec.groovy
@@ -0,0 +1,247 @@
+package com.pitchstone.plugin.flash
+
+import grails.test.mixin.support.GrailsUnitTestMixin
+import javax.crypto.spec.SecretKeySpec
+import javax.servlet.http.Cookie
+import org.apache.commons.logging.impl.SLF4JLogFactory
+import org.codehaus.groovy.grails.plugins.codecs.Base64Codec
+import spock.lang.Specification
+
+@Mixin(GrailsUnitTestMixin)
+class BasicSessionlessFlashCookieSpec extends Specification {
+    static final long JAN_1_2000 = 946684800000
+    static final long SEP_8_2001 = 1000000000000
+
+    def macAlgorithm = 'HMACSHA256'
+    def secret = new SecretKeySpec('secret'.bytes, macAlgorithm)
+    def cookie = new BasicSessionlessFlashCookie(
+        name: 'bs_flash',
+        maxAge: 300, // 5 minutes
+        macAlgorithm: macAlgorithm,
+        secret: secret,
+        log: new SLF4JLogFactory().getInstance(BasicSessionlessFlashCookie.class),
+    )
+
+    def setup() {
+        mockCodec Base64Codec
+    }
+
+    def "empty cookie can be created"() {
+        when: def cookie = new BasicSessionlessFlashCookie()
+        then:
+            cookie.name == ''
+            cookie.value == ''
+            cookie.path == null
+            cookie.domain == null
+            cookie.maxAge == -1
+            !cookie.secure
+            !cookie.httpOnly
+            cookie.macAlgorithm == null
+            cookie.secret == null
+            cookie.log == null
+            cookie.flash == [:]
+    }
+
+    def "cookie can be created from another cookie"() {
+        when:
+            def original = new Cookie('name', 'value')
+            original.path = 'path'
+            original.domain = 'domain'
+            original.maxAge = 1
+            original.secure = true
+            def cookie = new BasicSessionlessFlashCookie(original)
+        then:
+            cookie.name == 'name'
+            cookie.value == 'value'
+            cookie.path == 'path'
+            cookie.domain == 'domain'
+            cookie.maxAge == 1
+            cookie.secure
+            !cookie.httpOnly
+    }
+
+
+    def "format maxAge of 0 is Jan 1 1970"() {
+        expect: cookie.formatMaxAge(0) == 'Thu, 01-Jan-1970 00:00:00 GMT'
+    }
+
+    def "format maxAge of 300 is 5 minutes from now"() {
+        expect: cookie.formatMaxAge(300, JAN_1_2000) == 'Sat, 01-Jan-2000 00:05:00 GMT'
+    }
+
+
+    def "format RFC 3339 for null"() {
+        expect: cookie.formatRfc3339(null) == null
+    }
+
+    def "format RFC 3339 for Jan 1 2000 with milliseconds"() {
+        expect: cookie.formatRfc3339(new Date(JAN_1_2000)) == '2000-01-01T00:00:00.000Z'
+    }
+
+    def "format RFC 3339 for Jan 1 2000 without milliseconds"() {
+        expect: cookie.formatRfc3339(new Date(JAN_1_2000), false) == '2000-01-01T00:00:00Z'
+    }
+
+
+    def "parse RFC 3339 for non date"() {
+        expect: cookie.parseRfc3339('foo') == null
+    }
+
+    def "parse RFC 3339 for Jan 1 2000 with milliseconds"() {
+        expect: cookie.parseRfc3339('2000-01-01T00:00:00.000Z') == new Date(JAN_1_2000)
+    }
+
+    def "parse RFC 3339 for Jan 1 2000 without milliseconds"() {
+        expect: cookie.parseRfc3339('2000-01-01T00:00:00Z') == new Date(JAN_1_2000)
+    }
+
+
+    def "null flash converts to empty value"() {
+        when: cookie.flash = null
+        then: cookie.value == ''
+    }
+
+    def "empty flash converts to empty value"() {
+        when: cookie.flash = [:]
+        then: cookie.value == ''
+    }
+
+    def "one simple flash entry converts to one-entry value"() {
+        when: cookie.flash = [foo: 'bar']
+        then: cookie.getValue(JAN_1_2000) ==
+            'foo:bar|2000-01-01T00:00:00.000Z|TZkxbRtstY9s55XgheB0WI1KcCvrBaQqjYELReKiRwY'
+    }
+
+    def "three simple flash entries convert to three-entry value"() {
+        when: cookie.flash = [foo: 'bar', x: 'y', AC: 'DC']
+        then: cookie.getValue(JAN_1_2000) ==
+            'foo:bar|x:y|AC:DC|2000-01-01T00:00:00.000Z|UAAxTVW6flm0B5gwGZFBsKF6fV49k+ko+v9oWxV5SmM'
+    }
+
+    def "flash entries with non-alpha chars convert to encoded value"() {
+        when: cookie.flash = ['yes?': 'we\'re #1!*', 'CPT\u00ae': '$4 & 75%']
+        then: cookie.getValue(JAN_1_2000) ==
+            'yes%3F:we%27re+%231%21*|CPT%C2%AE:%244+%26+75%25|2000-01-01T00:00:00.000Z|X/DvQY3JBxZC+pFDMouHXXXR2X3Cg3Qr1OWHgqxIIUI'
+    }
+
+    def "flash can be double encoded"() {
+        when:
+            cookie.doubleEncode = true
+            cookie.flash = [foo: 'bar']
+        then: cookie.getValue(JAN_1_2000) ==
+            'Zm9vOmJhcnwyMDAwLTAxLTAxVDAwOjAwOjAwLjAwMFp8VFpreGJSdHN0WTlzNTVYZ2hlQjBXSTFLY0N2ckJhUXFqWUVMUmVLaVJ3WQ'
+    }
+
+    
+    def "null value converts to empty flash"() {
+        when: cookie.value = null
+        then: cookie.flash == [:]
+    }
+    
+    def "empty value converts to empty flash"() {
+        when: cookie.value = ''
+        then: cookie.flash == [:]
+    }
+    
+    def "value with not enough parts converts to empty flash"() {
+        when: cookie.value = 'foo'
+        then: cookie.flash == [:]
+
+        when: cookie.value = 'foo|bar'
+        then: cookie.flash == [:]
+
+        when: cookie.value = '|'
+        then: cookie.flash == [:]
+    }
+    
+    def "value with invalid mac converts to empty flash"() {
+        when: cookie.value = 'foo|bar|baz'
+        then: cookie.flash == [:]
+
+        when: cookie.value = '||'
+        then: cookie.flash == [:]
+
+        when: cookie.value = 'foo|bar|baz|xyz'
+        then: cookie.flash == [:]
+
+        when: cookie.value = '|||'
+        then: cookie.flash == [:]
+    }
+    
+    def "value with expired date converts to empty flash"() {
+        when: cookie.value =
+            'foo:bar|2000-01-01T00:00:00.000Z|TZkxbRtstY9s55XgheB0WI1KcCvrBaQqjYELReKiRwY'
+        then: cookie.getFlash(SEP_8_2001) == [:]
+    }
+    
+    def "one-entry value converts to one simple flash entry"() {
+        when: cookie.value =
+            'foo:bar|2000-01-01T00:00:00.000Z|TZkxbRtstY9s55XgheB0WI1KcCvrBaQqjYELReKiRwY'
+        then: cookie.getFlash(JAN_1_2000) == [foo: 'bar']
+    }
+    
+    def "three-entry value converts to three simple flash entries"() {
+        when: cookie.value =
+            'foo:bar|x:y|AC:DC|2000-01-01T00:00:00.000Z|UAAxTVW6flm0B5gwGZFBsKF6fV49k+ko+v9oWxV5SmM'
+        then: cookie.getFlash(JAN_1_2000) == [foo: 'bar', x: 'y', AC: 'DC']
+    }
+    
+    def "encoded value converts to flash entries with non-alpha chars"() {
+        when: cookie.value =
+            'yes%3F:we%27re+%231%21*|CPT%C2%AE:%244+%26+75%25|2000-01-01T00:00:00.000Z|X/DvQY3JBxZC+pFDMouHXXXR2X3Cg3Qr1OWHgqxIIUI'
+        then: cookie.getFlash(JAN_1_2000) == ['yes?': 'we\'re #1!*', 'CPT\u00ae': '$4 & 75%']
+    }
+
+    def "flash can be double unencoded"() {
+        when:
+            cookie.doubleEncode = true
+            cookie.value = 
+                'Zm9vOmJhcnwyMDAwLTAxLTAxVDAwOjAwOjAwLjAwMFp8VFpreGJSdHN0WTlzNTVYZ2hlQjBXSTFLY0N2ckJhUXFqWUVMUmVLaVJ3WQ'
+        then: cookie.getFlash(JAN_1_2000) == [foo: 'bar']
+    }
+
+
+    def "cancel cookie"() {
+        setup:
+            def added = false
+            def response = [ addCookie: { added = true } ]
+        when: cookie.cancel response
+        then:
+            added
+            cookie.maxAge == 0
+            cookie.path == '/'
+    }
+
+    
+    def "send basic cookie"() {
+        setup:
+            def headers = [:]
+            def response = [ addHeader: { k,v -> headers[k] = v } ]
+        when:
+            cookie.flash = [foo: 'bar']
+            cookie.send response, JAN_1_2000
+        then:
+            headers.size() == 1
+            headers.'Set-Cookie' ==
+                'bs_flash=foo:bar|2000-01-01T00:00:00.000Z|TZkxbRtstY9s55XgheB0WI1KcCvrBaQqjYELReKiRwY; expires=Sat, 01-Jan-2000 00:05:00 GMT'
+    }
+    
+    def "send custom cookie"() {
+        setup:
+            def headers = [:]
+            def response = [ addHeader: { k,v -> headers[k] = v } ]
+        when:
+            cookie.name = 'bsf'
+            cookie.path = '/app'
+            cookie.domain = 'example.com'
+            cookie.maxAge = 60
+            cookie.secure = true
+            cookie.httpOnly = true
+            cookie.flash = [foo: 'bar']
+            cookie.send response, JAN_1_2000
+        then:
+            headers.size() == 1
+            headers.'Set-Cookie' ==
+                'bsf=foo:bar|2000-01-01T00:00:00.000Z|TZkxbRtstY9s55XgheB0WI1KcCvrBaQqjYELReKiRwY; expires=Sat, 01-Jan-2000 00:01:00 GMT; domain=example.com; path=/app; secure; httponly'
+    }
+}
